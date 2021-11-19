17bdf0cb100f3753e673a67101e4cfd506ef409d
Muhammad Syukur Abadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-19 08:52:21 +0700

    fix: perbaikan migrasi
    perbaikan migrasi pada seluruh tabel pada database

--- api/urls.py
+++ api/urls.py
@@ -11,15 +11,13 @@ urlpatterns = [
     path('users/', UsersView.as_view()),
     path('user/detail/<int:user_id>', UserDetailView.as_view()),
     path('user/register/', UserRegister.as_view()),
-    path('user/login/', UserLogin.as_view()),
-    path('user/authenticate/', UserAuthentication.as_view()),
-    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/logout/', UserLogout.as_view()),
-    path('stations/', StationView.as_view()),
-    path('station/register/', StationRegister.as_view()),
-    path('station/detail/<int:station_id>', StationDetailView.as_view()),
-    path('cycles/', CycleView.as_view()),
-    path('cycle/register/', CycleRegistration.as_view()),
-    path('cycle/detail/<int:cycle_id>', CycleDetailView.as_view())
+    # path('stations/', StationView.as_view()),
+    # path('station/register/', StationRegister.as_view()),
+    # path('station/detail/<int:station_id>', StationDetailView.as_view()),
+    # path('cycles/', CycleView.as_view()),
+    # path('cycle/register/', CycleRegistration.as_view()),
+    # path('cycle/detail/<int:cycle_id>', CycleDetailView.as_view())
 ]
